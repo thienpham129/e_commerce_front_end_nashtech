0d53b2bece000578d1498decb3ae6ba029e1b2c1
thienpham129 <thienversiopp129.it.vn@gmail.com>
2025-04-23 15:02:33 +0700

    fix(product) - 01: fix remove ProductByCategoryPage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
-import UserList from './components/user/UserList';
 import ProductForUserList from './components/layout/productUserPage/ProductForUserList';
 import ProductDetails from './components/layout/productUserPage/ProductDetails';
-import ProductListByCategory from './components/layout/productUserPage/ProductListByCategory';
 
 
 function App() {
@@ -13,7 +11,6 @@ function App() {
         <Routes>
           <Route path='/user/products' element={<ProductForUserList />} />
           <Route path='/user/products/:productId' element={<ProductDetails />} />
-          <Route path='/user/products/category/:categoryId' element={<ProductListByCategory />} />
         </Routes>
       </div>
     </Router>
